dfdac1fcb9baf6fac32feb7b83431fe9a80fc6eb
Everton Zanini <107217906+everton-zanini@users.noreply.github.com>
2023-01-03 21:47:17 -0300

    00 BigPicture ok!

---
+++ 00_BigPicture/basic.ts
@@ -0,0 +1,19 @@
+class Customer{
+  private name:string;
+
+  constructor(name:string){
+    this.name = name;
+  }
+
+  announce(){
+    return "Hello, my name is " + this.name;
+  }
+}
+
+// create a new instance
+let firstCustomer = new Customer("Alice");
+let newMessage: string = firstCustomer.announce();
+
+// change the text on te page
+let webHeading = document.querySelector('h1');
+webHeading!.textContent = newMessage;
